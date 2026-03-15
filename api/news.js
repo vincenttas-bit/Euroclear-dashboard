361d5f478cd03c22520e9847b4dcc58466e086d6
Parmenides <vincenttas@hotmail.com>
2026-03-15 12:50:20 +0100

    Update news.js

--- api/news.js
+++ api/news.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
 try {
 
 const url =
-"https://api.gdeltproject.org/api/v2/doc/doc?query=Euroclear&mode=ArtList&maxrecords=100&format=json&sort=datedesc"
+"https://api.gdeltproject.org/api/v2/doc/doc?query=Euroclear&mode=ArtList&maxrecords=50&format=json&sort=datedesc"
 
 const response = await fetch(url, {
 headers: {
@@ -16,49 +16,11 @@ const data = await response.json()
 
 const articles = Array.isArray(data.articles) ? data.articles : []
 
-/* trusted sources */
-
-const allowedSources = [
-"reuters.com",
-"bloomberg.com",
-"ft.com",
-"politico.com",
-"politico.eu",
-"economist.com",
-"wsj.com",
-"nytimes.com",
-"washingtonpost.com",
-"cnn.com",
-"bbc.com",
-"bbc.co.uk",
-"theguardian.com",
-"elpais.com",
-"lemonde.fr",
-"spiegel.de",
-"ansa.it",
-"nikkei.com",
-"scmp.com",
-"aljazeera.com",
-"arabnews.com",
-"tass.com",
-"kyivpost.com",
-"coindesk.com",
-"globalcustodian.com"
-]
-
-let filtered = articles.filter(a => {
-
-const domain = (a.domain || "").replace("www.","")
-
-return allowedSources.includes(domain)
-
-})
-
 let russia = 0
 let sources = {}
 let topics = {Russia:0, Regulation:0, Technology:0, Digital:0}
 
-filtered.forEach(a => {
+articles.forEach(a => {
 
 const title = (a.title || "").toLowerCase()
 
@@ -80,17 +42,17 @@ sources[domain]++
 
 res.status(200).json({
 
-total: filtered.length,
+total: articles.length,
 
-russiaShare: filtered.length
-? (russia / filtered.length) * 100
+russiaShare: articles.length
+? (russia / articles.length) * 100
 : 0,
 
 sources: sources,
 
 topics: topics,
 
-articles: filtered.slice(0,10)
+articles: articles.slice(0,10)
 
 })
 
