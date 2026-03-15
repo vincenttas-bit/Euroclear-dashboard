7a695c3ff2123677a63f38a012ae270be083afd0
Parmenides <vincenttas@hotmail.com>
2026-03-15 09:18:10 +0100

    Update news.js

--- api/news.js
+++ api/news.js
@@ -1,18 +1,19 @@
 export default async function handler(req, res) {
 
-const url =
-"https://api.gdeltproject.org/api/v2/doc/doc?query=Euroclear&mode=ArtList&maxrecords=180&format=json";
-
 try {
 
-const response = await fetch(url);
-const data = await response.json();
+const url =
+"https://api.gdeltproject.org/api/v2/doc/doc?query=Euroclear&mode=ArtList&maxrecords=200&format=json"
 
-res.status(200).json(data);
+const response = await fetch(url)
+
+const data = await response.json()
+
+res.status(200).json(data)
 
 } catch (error) {
 
-res.status(500).json({ error: "GDELT request failed" });
+res.status(500).json({ error: "Failed to fetch GDELT data" })
 
 }
 
